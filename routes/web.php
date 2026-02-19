79e9d408b24addb9c3b194b310cb117d61499236
Andsan03 <andrewabellosanchez123456@gmail.com>
2026-02-19 12:49:22 +0100

    cambios

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,37 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [OrderController::class, 'index'])->name('index');
 
-Route::get('/client/create', [ClientController::class, 'create'])->name('client.create');
-Route::post('/client', [ClientController::class, 'store'])->name('client.store');
-Route::delete('/client/{id}', [ClientController::class, 'destroy'])->name('client.destroy');
 
-Route::get('/order/create', [OrderController::class, 'create'])->name('order.create');
-Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+//AUTENTICACION
+//Rutas sin autenticar
 
+Route::middleware(['guest'])->group(function () {
+
+
+});
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/order/create', [OrderController::class, 'create'])->name('order.create');
+    Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+    Route::get('/client/create', [ClientController::class, 'create'])->name('client.create');
+    Route::post('/client', [ClientController::class, 'store'])->name('client.store');
+    Route::delete('/client/{id}', [ClientController::class, 'destroy'])->name('client.destroy');
+
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+
+    Route::put('/dashboard/{id}', [UserController::class, 'update'])->name('update');
+
+    Route::post('/logout', function () {
+        auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect('/')->with('success', 'Sesión cerrada correctamente');
+    })->name('logout');
+});
